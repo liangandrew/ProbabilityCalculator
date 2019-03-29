b00ea54b7eb00aaa810f418e73e3dea58f464259
andrew <andrew.liang@stonybrook.edu>
2019-03-28 21:25:56 -0400

    fixed where int conversion occurs

--- Prob.py
+++ Prob.py
@@ -2,8 +2,7 @@ import math
 
 def choose(n,k):
     "Combination n choose k"
-    n = int(n)
-    k = int(k)
+
     top = math.factorial(n)
     bottom = math.factorial(k) * math.factorial(n - k)
     print(top/bottom)
@@ -11,8 +10,7 @@ def choose(n,k):
 
 def perm(n,k):
     "Permutation n p k"
-    n = int(n)
-    k = int(k)
+
     top = math.factorial(n)
     bottom = math.factorial(n-k)
     print(top/bottom)
@@ -27,8 +25,8 @@ while flag:
 
     if "c" in prob:
         ex = prob.split("c",1)
-        n = ex[0]
-        k = ex[-1]
+        n = int(ex[0])
+        k = int(ex[-1])
         if k > n:
             print("k cannot be greater than n")
         else:
@@ -36,8 +34,8 @@ while flag:
 
     if "p" in prob:
         ex = prob.split("p",1)
-        n = ex[0]
-        k = ex[-1]
+        n = int(ex[0])
+        k = int(ex[-1])
         if k > n:
             print("k cannot be greater than n")
         else:
